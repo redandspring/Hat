ddbb78e74d6374a574e7531e3304458da92c4f07
Tretyakov <tretyakov.developer@gmail.com>
2020-12-07 20:41:21 +0300

    Initial commit

---
+++ src/test/java/ru/redandspring/javaops/hat/HatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.redandspring.javaops.hat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
